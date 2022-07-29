604a5ed68ff5c95fc272fd8e528cd0daed6e887d
SupmaCat <65302212+KaizoKat@users.noreply.github.com>
2022-07-29 19:03:43 +0200

    V_0.2.3__Merginng
    Re- adds lost functionality lights. And merges it with the distance bugger. some lights don't work (the only one that does is the directional.) These changes will be  re-implemented later with better functionality like more maps, shadows and BLOOOOMMMM...

--- Papirus/Source Files/Main.cpp
+++ Papirus/Source Files/Main.cpp
@@ -16,7 +16,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
-unsigned int loadTexture(const char* path);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -38,6 +37,8 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 float moveSpeed;
+
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -81,6 +82,7 @@ int main()
     glDepthFunc(GL_LESS);
     
     stbi_set_flip_vertically_on_load(false);
+
     // build and compile our shader zprogram
     // ------------------------------------
     Shader ourShader("Papirus/Resource Files/Shaders/default.vrs", "Papirus/Resource Files/Shaders/default.frs");
@@ -88,6 +90,10 @@ int main()
     // load models
     // -----------
 
+
+    ourShader.use();
+    ourShader.setInt("material.diffuse", 0);
+    ourShader.setInt("material.specular", 1);
 
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -112,7 +118,14 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // don't forget to enable shader before setting uniforms
-        ourShader.use();
+        ourShader.use();;
+        ourShader.setVec3("viewPos", camera.Position);
+        ourShader.setFloat("material.shininess", 1.0f);
+
+        ourShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        ourShader.setVec3("dirLight.ambient", 0.8f, 0.8f, 0.8f);
+        ourShader.setVec3("dirLight.diffuse", 0.8f, 0.8f, 0.8f);
+        ourShader.setVec3("dirLight.specular", 0.3f, 0.3f, 0.3f);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.001f, 300.0f);
@@ -126,6 +139,8 @@ int main()
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         ourModel.Draw(ourShader);
+
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
